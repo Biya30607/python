3d1f617da85b308e27ab43cf63c3bc0a206a39cc
Biya thanzeen <thanzeenbiya@gmail.com>
2025-11-25 13:59:40 +0530

    Add files via upload

---
+++ builtin co3.py
@@ -0,0 +1,8 @@
+import math
+import datetime
+import random
+print("Square root of 16:", math.sqrt(16))
+print("PI:", math.pi)
+now = datetime.datetime.now()
+print("Current time:", now)
+print("Random number 1–10:", random.randint(1, 10))
